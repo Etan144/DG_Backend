762d7ad7737dc69f2c1977a41d61fba168d48af9
Etan144 <eugenetan310101@gmail.com>
2025-12-22 03:02:42 +0800

    fix main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.auth import router as auth_router
 from app.api.health import router as health_router
-from app.api.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_name)
