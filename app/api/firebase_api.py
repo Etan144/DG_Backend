fa9bc56628e8d3b9cb36dfd7e93663b9060ef2ef
Etan144 <eugenetan310101@gmail.com>
2026-01-05 00:06:44 +0800

    fix1

--- app/api/firebase_api.py
+++ app/api/firebase_api.py
@@ -7,7 +7,7 @@ router = APIRouter(prefix="/firebase",tags=["Firebase"])
 
 @router.post("/token")
 def get_firebase_token(current_user=Depends(get_current_user)):
-    firebase_token = auth.create_custom_token(current_user.id)
+    firebase_token = auth.create_custom_token(str(current_user.id))
     return {
         "firebase_token": firebase_token.decode("utf-8")
     }
